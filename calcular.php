2b566e884c99d0bba0dcbc443b1b97440c41d6cb
Thaty <thaty.s.a@hotmail.com>
2020-05-27 17:15:13 -0300

    no message

--- calcular.php
+++ calcular.php
@@ -12,31 +12,38 @@
     $idade  = $_POST['idade'];
     $peso   = $_POST['peso'];
     $altura = $_POST['altura'];
-    
-  function categoria(){
-    $abaixo = 'Abaixo do Peso';
-    $normal = 'Peso Normal';
-    $sobrepeso = 'Sobrepesso';
-    $obesidade1 = 'Obesidade Grau 1';
-    $obesidade2 = 'Obesidade Grau 2';
-    $obesidade3 = 'Obesidade Grau 3';
 
-    if ($resultado < 18,5) {
-        echo '<p>Seu IMC é' .$resultado 'e está </p>' .$abaixo;
+    function calculo(){
+      $conta1 = $altura*$altura;
+	  	$conta2 = $peso/$conta1;
+		
+	    $resultado = number_format($conta2);
     }
-    elseif($resultado >= 18,5 or $resultado <= 24,9){
-        echo '<p>Seu IMC é' .$resultado 'e está </p>' .$normal;
+
+    function categoria(){
+      $abaixo = 'Abaixo do Peso';
+      $normal = 'Peso Normal';
+      $sobrepeso = 'Sobrepesso';
+      $obesidade1 = 'Obesidade Grau 1';
+      $obesidade2 = 'Obesidade Grau 2';
+      $obesidade3 = 'Obesidade Grau 3';
+
+      if ($resultado < 18,5) {
+          echo '<p>Seu IMC é' .$resultado 'e está </p>' .$abaixo;
+      }
+      elseif($resultado >= 18,5 or $resultado <= 24,9){
+          echo '<p>Seu IMC é' .$resultado 'e está </p>' .$normal;
+      }
+      elseif($resultado >= 25,0 or $resultado <= 29,9){
+          echo '<p>Seu IMC é' .$resultado 'e está </p>' .$sobrepeso;
+      }
+      elseif($resultado >= 30,0 or $resultado <= 34,9){
+        echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade1;
+      }
+      elseif($resultado >= 35,0 or $resultado <= 39,9){
+          echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade2;
+      }
+      elseif($resultado >= 40,0){
+        echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade3;
+      }
     }
-    elseif($resultado >= 25,0 or $resultado <= 29,9){
-        echo '<p>Seu IMC é' .$resultado 'e está </p>' .$sobrepeso;
-    }
-    elseif($resultado >= 30,0 or $resultado <= 34,9){
-       echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade1;
-    }
-    elseif($resultado >= 35,0 or $resultado <= 39,9){
-        echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade2;
-    }
-    elseif($resultado >= 40,0){
-       echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade3;
-    }
-  }
