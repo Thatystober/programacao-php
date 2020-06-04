649927ecbe23c8011db044a423e0338ee80f7e39
Thaty <thaty.s.a@hotmail.com>
2020-06-03 21:30:29 -0300

    fico lindu

--- calcular.php
+++ calcular.php
@@ -2,35 +2,33 @@
 <html lang="pt-br">
   <head>
 	<meta charset="UTF-8"/>
-	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<link href="css/calcular.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet">
 	<title>Resultado IMC</title>
   </head>
 <body> 
 <?php
-
+    error_reporting(0);
     $nome   = $_POST['nome1'];
-    $idade  = $_POST['idade1'];
-    $peso   = $_POST['peso1'];
-    $altura = $_POST['altura1'];
+    $idade  = intval($_POST['idade1']);
+    $peso   = floatval($_POST['peso1']);
+    $altura = floatval($_POST['altura1']);
 
     $nome2 = $_POST['nome2'];
-    $idade2 = $_POST['idade2'];
-    $peso2 = $_POST['peso2'];
-    $altura2 = $_POST['altura2';]
+    $idade2 = intval($_POST['idade2']);
+    $peso2 = floatval($_POST['peso2']);
+    $altura2 = floatval($_POST['altura2']);
 
     $nome3 = $_POST['nome3'];
-    $idade3 = $_POST['idade3'];
-    $peso3 = $_POST['peso3'];
-    $altura2 = $_POST['altura3';]
+    $idade3 = intval($_POST['idade3']);
+    $peso3 = floatval($_POST['peso3']);
+    $altura3 = floatval($_POST['altura3']);
 
-    function calculo(){
-      $conta1 = $altura*$altura;
-	  	$conta2 = $peso/$conta1;
-		
-	    $resultado = number_format($conta2);
-    }
+    $resultado1 = $peso / ($altura ** 2);
+    $resultado2 = $peso2 / ($altura2 ** 2);
+    $resultado3 = $peso3 / ($altura3 ** 2);
 
-    function categoria(){
+    function categorias($var){
       $abaixo = 'Abaixo do Peso';
       $normal = 'Peso Normal';
       $sobrepeso = 'Sobrepesso';
@@ -38,23 +36,52 @@
       $obesidade2 = 'Obesidade Grau 2';
       $obesidade3 = 'Obesidade Grau 3';
 
-      if ($resultado1 && $resultado2 & $resultado3 =< 18,5) {
-          echo '<p>Seu IMC é' .$resultado 'e está </p>' .$abaixo;
+      if ($var <= 18.5){
+        return $abaixo;
       }
-      elseif($resultado1 && $resultado2 & $resultado3 >= 18,5 or $resultado1 && $resultado2 & $resultado3 <= 24,9){
-          echo '<p>Seu IMC é' .$resultado 'e está </p>' .$normal;
+      else if ($var >= 18.5 && $var <= 24.9 ){
+        return $normal;
       }
-      elseif($resultado1 && $resultado2 & $resultado3 >= 25,0 or $resultado1 && $resultado2 & $resultado3 <= 29,9){
-          echo '<p>Seu IMC é' .$resultado 'e está </p>' .$sobrepeso;
+      else if ($var >= 25.0 && $var  <= 29.9){
+        return $sobrepeso;
       }
-      elseif($resultado1 && $resultado2 & $resultado3 >= 30,0 or $resultado1 && $resultado2 & $resultado3 <= 34,9){
-        echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade1;
-        // mudar os echos (?)
+      else if ($var >= 30.0 && $var <= 35.0){
+        return $obesidade1;
       }
-      elseif($resultado1 && $resultado2 & $resultado3 >= 35,0 or $resultado1 && $resultado2 & $resultado3 <= 39,9){
-          echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade2;
+      else if ($var >= 35.0 && $var <= 39.9){
+        return $obesidade2;
       }
-      elseif($resultado1 && $resultado2 & $resultado3 >= 40,0){
-        echo '<p>Seu IMC é' .$resultado 'e está </p>' .$obesidade3;
-      }
+      else if ($var >= 40.0){
+        return $obesidade3;
+      } 
     }
+
+    $imc = ($resultado1 + $resultado2 + $resultado3) / 3;
+    $mediaIdade = ($idade + $idade2 + $idade3) / 3;
+
+    ?>
+    <div id="resultado">
+      <h1 class="usuario">Usuário 1</h1>
+      <p><?php echo $nome ?> <?php echo $idade ?> anos</p>
+      <p>Seu imc é <?php echo $resultado1 ?> e você está com <?php echo categorias($resultado1)?></p>
+      <p>Resultado obtido a partir do peso <?php echo $peso ?> e da altura <?php echo $altura ?></p>
+
+      <h1 class="usuario" >Usuário 2</h1>
+      <p><?php echo $nome2 ?> <?php echo $idade2 ?> anos</p>
+      <p>Seu imc é <?php echo $resultado2 ?> e você está com <?php echo categorias($resultado2)?></p>
+      <p>Resultado obtido a partir do peso <?php echo $peso2 ?> e da altura <?php echo $altura2 ?></p>
+
+      <h1 class="usuario">Usuário 3</h1>
+      <p><?php echo $nome3 ?> <?php echo $idade3?> anos</p>
+      <p>Seu imc é <?php echo $resultado3 ?> e você está com <?php echo categorias($resultado3)?></p>
+      <p>Resultado obtido a partir do peso <?php echo $peso3 ?> e da altura <?php echo $altura3 ?></p>
+
+      <h2 id="media">Média dos IMCS e Idade dos Usuários</h2>
+      <p>A média de imc dos usuários é <?php echo $imc ?> corresponde à categoria <?php echo categorias($var)?></p>
+      <p>A média de idade é <?php echo $mediaIdade ?> anos</p>
+
+      <button><a href="index.php">Voltar</a></button>
+    </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet">
     <title>Calcule seu IMC</title>
 </head>
 
@@ -19,16 +20,16 @@
             <div id="divisao">
                 <h2>Utilize os campos abaixo para calcular o seu IMC.</h2>
 
-                <form class="imc-form" action="function.php" method="post">
+                <form class="imc-form" action="calcular.php" method="post">
                     <div id="us-1">
                         <p>Usuário 1</p>
                         <input type="text" name="nome1" placeholder="Digite seu nome" require>
 
                         <input type="number" name="idade1" placeholder="Digite sua idade" require>
 
-                        <input type="number" name="peso1" placeholder="Digite seu peso"  require>
+                        <input type="number" name="peso1" step="0.01" placeholder="Digite seu peso"  require>
 
-                        <input type="number" name="altura1" placeholder="Digite sua altura"  require>
+                        <input type="number" name="altura1" step="0.01" placeholder="Digite sua altura"  require>
                     </div>
                     <div id="us-2">
                         <p>Usuário 2</p>
@@ -36,9 +37,9 @@
 
                         <input type="number" name="idade2" placeholder="Digite sua idade" require>
 
-                        <input type="number" name="peso2" placeholder="Digite seu peso" require>
+                        <input type="number" name="peso2" step="0.01" placeholder="Digite seu peso" require>
                         
-                        <input type="number" name="altura2" placeholder="Digite sua altura"  require>
+                        <input type="number" name="altura2" step="0.01" placeholder="Digite sua altura"  require>
                     </div>
                     <div id="us-3">
                         <p>Usuário 3</p>
@@ -46,14 +47,15 @@
 
                         <input type="number" name="idade3" placeholder="Digite sua idade" require>
 
-                        <input type="number" name="peso3" placeholder="Digite seu peso" require>
+                        <input type="number" name="peso3" step="0.01"  placeholder="Digite seu peso" require>
                         
-                        <input type="number" name="altura3" placeholder="Digite sua altura" require>
+                        <input type="number" name="altura3" step="0.01" placeholder="Digite sua altura" require>
+                    </div>
+
+                    <div id="botoes"> 
+                        <input id="calcular" type="submit" value="Calcular">
                     </div>
                 </form>
-                <div id="botoes"> 
-                    <input id="calcular" type="button" value="Calcular">
-                </div>
             </div>
 
             <section class="tab">
